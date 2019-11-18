a1775982fa41caa70478d8cca8a41e0b8fe6955e
Eneroth3 <eneroth3@gmail.com>
2019-11-18 22:18:37 +0100

    Extract inference lock to mixin module

---
+++ inference_example_tool.rb
@@ -0,0 +1,111 @@
+require_relative("tool_inference")
+
+# Example tool showing how ToolInference mixin can be used.
+class InferenceExampleTool
+  include ToolInference
+
+  # @api
+  # @see https://ruby.sketchup.com/Sketchup/Tool.html
+  def initialize
+    @ip = Sketchup::InputPoint.new
+    @ip_reference = Sketchup::InputPoint.new
+
+    super
+  end
+
+  # @api
+  # @see https://ruby.sketchup.com/Sketchup/Tool.html
+  def deactivate(view)
+    reset(view)
+    view.invalidate
+  end
+
+  # @api
+  # @see https://ruby.sketchup.com/Sketchup/Tool.html
+  def draw(view)
+    if @ip_reference.valid?
+      view.line_width = view.inference_locked? ? 3 : 1
+      view.set_color_from_line(@ip.position, @ip_reference.position)
+      view.draw(GL_LINES, [@ip.position, @ip_reference.position])
+    end
+
+    view.line_width = 1
+    # FIXME: @ip_reference keeps drawing with inference helper lines.
+    # This is not how native tools draw InputPoints.
+    @ip_reference.draw(view)
+    @ip.draw(view)
+    view.tooltip = @ip.tooltip
+  end
+
+  # @api
+  # @see https://ruby.sketchup.com/Sketchup/Tool.html
+  def getExtents
+    bb = Sketchup.active_model.bounds
+    bb.add(@ip.position) if @ip.valid?
+    bb.add(@ip_reference.position) if @ip_reference.valid?
+
+    bb
+  end
+
+  # @api
+  # @see https://ruby.sketchup.com/Sketchup/Tool.html
+  def onCancel(_reason, view)
+    reset(view)
+    view.invalidate
+  end
+
+  # @api
+  # @see https://ruby.sketchup.com/Sketchup/Tool.html
+  def onLButtonDown(_flags, _x, _y, view)
+    if !@ip_reference.valid?
+      @ip_reference.copy!(@ip)
+    else
+      reset(view)
+    end
+    view.invalidate
+  end
+
+  # @api
+  # @see https://ruby.sketchup.com/Sketchup/Tool.html
+  def onMouseMove(_flags, x, y, view)
+    @ip.pick(view, x, y)
+    view.invalidate
+  end
+
+  # @api
+  # @see https://ruby.sketchup.com/Sketchup/Tool.html
+  def resume(view)
+    view.invalidate
+  end
+
+  # @api
+  # @see https://ruby.sketchup.com/Sketchup/Tool.html
+  def suspend(view)
+    view.invalidate
+  end
+
+  # @api
+  # @see `ToolInference`
+  def current_ip
+    @ip
+  end
+
+  # @api
+  # @see `ToolInference`
+  def start_ip
+    @ip_reference
+  end
+
+  private
+
+  def reset(view)
+    @ip.clear
+    @ip_reference.clear
+    view.lock_inference
+  end
+
+  # TODO: Pick inputpoint anew with same X and Y when keys are pressed or let
+  # go.
+end
+
+Sketchup.active_model.select_tool(InferenceExampleTool.new)
---
+++ tool_inference.rb
@@ -0,0 +1,102 @@
+# Mixin for inference lock in tools.
+#
+# Requires the method `input_point` to be defined and return whatever
+# input_point is currently  relevant for the tool.
+#
+# If initialize, onKeyDown or onKeyUp are overridden, call super in them.
+module ToolInference
+  # @api
+  # @see https://ruby.sketchup.com/Sketchup/Tool.html
+  def activate
+    @axis_lock = nil
+  end
+
+  # @api
+  # @see https://ruby.sketchup.com/Sketchup/Tool.html
+  def onKeyDown(key, _repeat, _flags, view)
+    return unless inference_lock_enabled?
+
+    case key
+    when CONSTRAIN_MODIFIER_KEY
+      view.lock_inference(current_ip)
+    when VK_RIGHT
+      lock_inference_axis([start_ip.position, view.model.axes.xaxis], view)
+    when VK_LEFT
+      lock_inference_axis([start_ip.position, view.model.axes.yaxis], view)
+    when VK_UP
+      lock_inference_axis([start_ip.position, view.model.axes.zaxis], view)
+    end
+
+    view.invalidate
+  end
+
+  # @api
+  # @see https://ruby.sketchup.com/Sketchup/Tool.html
+  def onKeyUp(key, _repeat, _flags, view)
+    return unless inference_lock_enabled?
+
+    return unless key == CONSTRAIN_MODIFIER_KEY
+    return if @axis_lock
+
+    view.lock_inference
+    view.invalidate
+  end
+
+  # Determine whether inference locking should be enabled.
+  # In certain tool states you may not want to allow inference locking.
+  #
+  # This method MAY be overridden with a method in your tool class.
+  #
+  # @return [Boolean] Defaults to `true`.
+  def inference_lock_enabled?
+    # TODO: Separate look to active and look to start.
+    # Native line tool no longer has axis inference until after the
+    # starting point is set, since SketchUp 2017 or so.
+    true
+  end
+
+  # Get reference to currently active InputPoint.
+  #
+  # This method MUST be overridden with a method in your tool class.
+  #
+  # @return [Sketchup::InputPoint]
+  def current_ip
+    raise NotImplementedError "Override this method in class using mixin."
+  end
+
+  # Get reference to currently InputPoint of operation start.
+  #
+  # This method MAY be overridden with a method in your tool class.
+  #
+  # @return [Sketchup::InputPoint] Defaults to `current_ip`.
+  def start_ip
+    current_ip
+  end
+
+  private
+
+  # Unlock inference lock to axis if there is any.
+  #
+  # @param view [Sketchup::view]
+  def unlock_axis(view)
+    # Any inference lock not done with `lock_inference_axis`, e.g. to the
+    # tool's primary InputPoint, should be kept.
+    return unless @axis_lock
+    @axis_lock = nil
+    view.lock_inference
+  end
+
+  # Lock inference to an axis or unlock if already locked to that very axis.
+  #
+  # @param line [Array<(Geom::Point3d, Geom::Vector3d)>]
+  # @param view [Sketchup::View]
+  def lock_inference_axis(line, view)
+    return unlock_axis(view) if line == @axis_lock
+
+    @axis_lock = line
+    view.lock_inference(
+      Sketchup::InputPoint.new(line[0]),
+      Sketchup::InputPoint.new(line[0].offset(line[1]))
+    )
+  end
+end
